2ed260bdcc1fe7df5f6191511ba6c94368a4fec9
Louis Jordan <louisjoecodes@gmail.com>
2025-11-11 18:13:13 +0000

    feat: add announcement

--- apps/www/app/(app)/(root)/page.tsx
+++ apps/www/app/(app)/(root)/page.tsx
@@ -13,6 +13,7 @@ import { PageNav } from "@/components/page-nav"
 import { ThemeSelector } from "@/components/theme-selector"
 import Speaker01 from "@/registry/elevenlabs-ui/blocks/speaker-01/page"
 import { Button } from "@/registry/elevenlabs-ui/ui/button"
+import { Announcement } from "@/components/announcement"
 
 const title = "ElevenLabs UI"
 const description =
@@ -57,7 +58,9 @@ export default function IndexPage() {
           }}
         />
         <PageHeader className="relative z-10">
+        <Announcement />
           <PageHeaderHeading className="max-w-4xl">
+         
             <span className="flex items-baseline gap-2 sm:gap-3">
               <span className="font-waldenburg-ht leading-[0.95] font-bold tracking-[-0.03em]">
                 ElevenLabs
